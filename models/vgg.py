b831043f88f41849811f706b69eeea3ab4bba985
绝世无敌妖王 <74641795+a1309783533@users.noreply.github.com>
2021-09-08 16:31:42 +0800

    Update vgg.py

--- models/vgg.py
+++ models/vgg.py
@@ -16,6 +16,7 @@ import torch
 import torch.nn as nn
 
 #通过字典定义需要构造的VGG网络类型，数字表示卷积层的特征数量，M表示最大池化层
+
 cfg = {
     'A' : [64,     'M', 128,      'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
     'B' : [64, 64, 'M', 128, 128, 'M', 256, 256,           'M', 512, 512,           'M', 512, 512,           'M'],
@@ -26,16 +27,16 @@ cfg = {
 class VGG(nn.Module):
     
 #features表示从卷积层提取的特征，num_class表示分类器的分类标签数目
+
     def __init__(self, features, num_class=100):
         super().__init__()
         self.features = features
 
-#
-Sequential是一个顺序容器,模块将按照它们在构造函数中传递的顺序添加到其中,之后的forward()方法接受任何输入并将其转发到它包含的第一个模块，
-然后它将输出“链接”到每个后续模块的输入，最后返回最后一个模块的输出.
-ReLU的参数inplace表示是否就地执行ReLU操作.
-Dropout有一个参数p,表示进行神经元丢弃的概率.
-#
+#Sequential是一个顺序容器,模块将按照它们在构造函数中传递的顺序添加到其中,之后的forward()方法接受任何输入并将其转发到它包含的第一个模块，
+#然后它将输出“链接”到每个后续模块的输入，最后返回最后一个模块的输出.
+#ReLU的参数inplace表示是否就地执行ReLU操作.
+#Dropout有一个参数p,表示进行神经元丢弃的概率.
+
         self.classifier = nn.Sequential(
             nn.Linear(512, 4096),
             nn.ReLU(inplace=True),
@@ -47,6 +48,7 @@ Dropout有一个参数p,表示进行神经元丢弃的概率.
         )
 
  #view函数实现将B,C,W,H的张量转换为B*(c*w*h)维的矩阵，方便输入全连接层进行分类
+
     def forward(self, x):
         output = self.features(x)
         output = output.view(output.size()[0], -1)
@@ -54,7 +56,8 @@ Dropout有一个参数p,表示进行神经元丢弃的概率.
 
         return output
 
-#
+#自定义特征提取层，即卷积层,将layer定义为一个列表,并且其后的Sequential如果遇到列表的输入，前必须加*，将输入迭代器拆成元素
+
 def make_layers(cfg, batch_norm=False):
     layers = []
 
